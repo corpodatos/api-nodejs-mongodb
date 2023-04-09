7e76761ca6a320e6377e9e17b85132108016c83f
corpodatos <edwinrivero@yahoo.com>
2023-04-08 23:49:15 -0400

    correción de conexion a bd

--- database/config.js
+++ database/config.js
@@ -2,18 +2,17 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   try {
-    mongoose.connect(process.env.MONGODB_CNN);
-    console.log(process.env.MONGODB_CNN);
-    // await mongoose.connect(process.env.MONGODB_CNN, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    //   useCreateIndex: true,
-    //   useFindAndModify: false,
-    // });
-    console.log('Base de datos online');
+    await mongoose.connect(process.env.MONGODB_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //useCreateIndex: true,
+      //useFindAndModify: false,
+    });
+
+    console.log("Base de datos online");
   } catch (error) {
-    console.log();
-    throw new Error("Error en conexión a BD");
+    console.log(error);
+    throw new Error("Error a la hora de iniciar la base de datos");
   }
 };
 
